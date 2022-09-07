bbec8324866ae7a7bcca42c349b957235d4f014e
Parisa Sohrabi <parisahs@gmail.com>
2022-09-07 16:15:20 -0400

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  const [users, setUsers] = useState([]);
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    const newUsers = [...users];
+    newUsers.push({ fname, lname, email, password, cPassword });
+    setUsers(newUsers);
+    setFname("");
+    setLname("");
+    setEmail("");
+    setPassword("");
+    setCPassword("");
+  };
+  return (
+    <form action="" className="form" onSubmit={formHandler}>
+      <div className="input-group">
+        <label htmlFor="">First Name</label>
+        <input
+          type="text"
+          value={fname}
+          onChange={(e) => setFname(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="">Last Name</label>
+        <input
+          type="text"
+          value={lname}
+          onChange={(e) => setLname(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="">Email</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="">Confirm Password</label>
+        <input
+          type="password"
+          value={cPassword}
+          onChange={(e) => setCPassword(e.target.value)}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/components/FormData.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FormData = () => {
+  return (
+    <div className="form-data">
+      <h2>your Form Data</h2>
+      <ul>
+        <li>
+          <h3>First Name</h3>
+          <h3>Last Name</h3>
+          <p>Email</p>
+          <p>Password</p>
+          <p>Confirm Password</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default FormData;
